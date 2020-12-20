46d090dfada78e60387dba0bfc7ca987d9d867ca
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2020-12-20 14:45:54 +0100

    Update Assets and start Character movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public Vector3 mousePos;
+    public Vector3 worldPos;
+    Vector3 playerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+        worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        gameObject.transform.position = new Vector3(worldPos.x, worldPos.y, gameObject.transform.position.z);
+    }
+}
---
+++ Assets/SimpleCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCamera : MonoBehaviour
+{
+    //How do I do this?
+
+    public GameObject mainCam;
+    private float posRight = 3.65f;
+    private float posLeft = -3.65f;
+    public float screenWidth = 1280; 
+
+
+    public void OnMouseDown()
+    {
+        float range = Mathf.Abs(posLeft) + posRight;
+
+        print(Input.mousePosition); // in realtion to screen 
+        float relation = Input.mousePosition.x / screenWidth;
+        print (relation); 
+        if( relation > 0.7f)
+        {
+            mainCam.gameObject.transform.position = new Vector3(posRight, mainCam.gameObject.transform.position.y, mainCam.gameObject.transform.position.z);
+        }
+        else if (relation < 0.3f)
+        {
+            mainCam.gameObject.transform.position = new Vector3(posLeft, mainCam.gameObject.transform.position.y, mainCam.gameObject.transform.position.z);
+        }
+
+       
+    }
+}
